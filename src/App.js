f1d74615aee5afec2e9ca0d3fb23251c0a649470
Shahmir Minhas <62132242+shahmir-minhas@users.noreply.github.com>
2022-09-06 11:51:30 +0500

    useCallback examole

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Child from "./Child";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
   const [childCount, setChildCount] = useState(0);
 
+  // using useCallback
+  const memoIncChild = useCallback((inc) => {
+    incrementChildCount(inc);
+  }, []);
+
   const incrementCount = () => {
     setCount(count + 1);
   };
-  const incrementChildCount = () => {
-    setChildCount(childCount + 1);
+
+  const incrementChildCount = (inc) => {
+    setChildCount(inc);
   };
+
   console.count("Parent render");
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Parent: {count}</p>
-        <button onClick={incrementCount}>increntment {count}</button>
-        <Child childCount={childCount} />
+        <button onClick={incrementCount}>increntment</button>
+
+        <Child childCount={childCount} incrementChildCount={memoIncChild} />
         <br />
-        <button onClick={incrementChildCount}>Child Inc Parent</button>
+        <button onClick={() => incrementChildCount(childCount + 1)}>
+          Child Inc Parent
+        </button>
       </header>
     </div>
   );
--- src/Child.jsx
+++ src/Child.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 
-const Child = ({ childCount }) => {
+const Child = ({ childCount, incrementChildCount }) => {
   console.count("Child render");
   return (
     <>
       <div>
         <p>Child State: {childCount}</p>
-        <button>Increment Child</button>
+        <button onClick={() => incrementChildCount(childCount + 1)}>
+          Increment Child
+        </button>
       </div>
     </>
   );
 };
 
 export default React.memo(Child);
+
+// passing func as props breaks memo
+// i-e  export default React.memo(Child);
+// each time child is rendered it creates a new referance/instance of incrementChildCount func
+// to avoid rerendering we must memorize incrementChildCount func
