9da2a1632d80393c908328470644ea080f807f65
Shahmir Minhas <62132242+shahmir-minhas@users.noreply.github.com>
2022-09-06 12:24:13 +0500

    how use memo works

--- src/Child.jsx
+++ src/Child.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 const Child = ({ childCount, incrementChildCount }) => {
+  //==================================
+  //                creating delay, everytime child rerender it computes
+  //====================================
+//   let computing = 0;
+//   for (let i = 0; i < 500000000; i++) {
+//     computing++;
+//   }
+  //==================================
+  //                using use memo
+  //====================================
+    const computing = useMemo(() => {
+      let computing = 0;
+      for (let i = 0; i < 500000000; i++) {
+        computing++;
+      }
+      return computing;
+    }, []);
   console.count("Child render");
+
   return (
     <>
       <div>
         <p>Child State: {childCount}</p>
+        <p>Computing: {computing.toLocaleString()}</p>
         <button onClick={incrementChildCount}>Increment Child</button>
       </div>
     </>
